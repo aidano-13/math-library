bfe1654d02b54da54c28fc3c37d43696e43a8092
Aidan <52678943+aidano-13@users.noreply.github.com>
2020-12-31 15:55:11 -0600

    ➕ Added Point class and geometry package.

--- src/com/github/aidano13/mathLibrary/equations/Equation.java
+++ src/com/github/aidano13/mathLibrary/equations/Equation.java
@@ -5,8 +5,8 @@ import com.github.aidano13.mathLibrary.errors.InadequateAmountOfNumbers;
 public interface Equation {
 
 	public float get(float x);
-	
+
 	public Equation pointsToLine(float[][] points) throws InadequateAmountOfNumbers;
-	
+
 	public String toString();
 }
---
+++ src/com/github/aidano13/mathLibrary/geometry/Point.java
@@ -0,0 +1,17 @@
+package com.github.aidano13.mathLibrary.geometry;
+
+public class Point {
+
+    public float x;
+    public float y;
+
+    public Point(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+}
