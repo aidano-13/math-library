ccacb4beb388115ff702579c4dee3e16ae578976
Aidan <52678943+aidano-13@users.noreply.github.com>
2020-12-31 16:01:02 -0600

    ➕ Changed pointsToLine methods to static methods.

--- src/com/github/aidano13/mathLibrary/equations/LinearEquation.java
+++ src/com/github/aidano13/mathLibrary/equations/LinearEquation.java
@@ -3,20 +3,38 @@ package com.github.aidano13.mathLibrary.equations;
 import com.github.aidano13.mathLibrary.errors.InadequateAmountOfNumbers;
 
 public class LinearEquation implements Equation {
-	
+
+	public static Equation pointsToLine(float[][] points) throws InadequateAmountOfNumbers {
+		if (points.length >= 2) {
+			float x1 = points[0][0];
+			float y1 = points[0][1];
+			float x2 = points[1][0];
+			float y2 = points[1][1];
+
+			float m = (y2 - y1) / (x2 - x1);
+
+			float b = y1 - m * x1;
+
+			return new LinearEquation(m, b);
+		} else {
+			throw new InadequateAmountOfNumbers(
+					String.format("Expected two or more points, instead found %o", points.length));
+		}
+	}
+
 	public float m;
 	public float b;
-	
+
 	public LinearEquation(float m, float b) {
 		this.m = m;
 		this.b = b;
 	}
-	
+
 	public LinearEquation(float m) {
 		this.m = m;
 		this.b = 0;
 	}
-	
+
 	public LinearEquation() {
 		this.m = 0;
 		this.b = 0;
@@ -26,27 +44,9 @@ public class LinearEquation implements Equation {
 	public float get(float x) {
 		return m * x + b;
 	}
-	
+
 	@Override
 	public String toString() {
 		return String.format("y = %fx + %f", m, b);
-	}
-
-	@Override
-	public Equation pointsToLine(float[][] points) throws InadequateAmountOfNumbers {
-		if (points.length >= 2) {
-			float x1 = points[0][0];
-			float y1 = points[0][1];
-			float x2 = points[1][0];
-			float y2 = points[1][1];
-			
-			float m = (y2 - y1) / (x2 - x1);
-			
-			float b = y1 - m * x1;
-			
-			return new LinearEquation(m, b);
-		} else {
-			throw new InadequateAmountOfNumbers(String.format("Expected two or more points, instead found %o", points.length));
-		}
 	}
 }
