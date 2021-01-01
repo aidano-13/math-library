e5cd5151629c4905dd3504099b6857f14279d588
Aidan <52678943+aidano-13@users.noreply.github.com>
2020-12-31 19:41:58 -0600

    ➕ Added Statistics class.

---
+++ src/com/github/aidano13/mathLibrary/statistics/Statistics.java
@@ -0,0 +1,20 @@
+package com.github.aidano13.mathLibrary.statistics;
+
+public class Statistics {
+
+    public static float mean() {
+        return 0;
+    }
+
+    public static float median() {
+        return 0;
+    }
+
+    public static float mode() {
+        return 0;
+    }
+
+    public static float range() {
+        return 0;
+    }
+}
